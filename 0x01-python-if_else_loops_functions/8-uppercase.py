a04b284f77f1b69307e59951789680df7a3f9280
robertrowe1013 <robertrowe1013@gmail.com>
2020-04-29 19:35:04 +0000

    changed print statements to string fomrat

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for elem in str:
         if ord(elem) > 96 and ord(elem) < 123:
             elem = (chr(ord(elem) - 32))
-        print(elem, end='')
-    print('\n', end='')
+        print("{}".format(elem), end='')
+    print("\n", end='')
