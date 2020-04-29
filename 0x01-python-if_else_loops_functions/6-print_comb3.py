5ad442f95251f73ce875918dc92a36a239a042f9
robertrowe1013 <robertrowe1013@gmail.com>
2020-04-29 19:11:14 +0000

    tast 6 complete

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-for i in range(100):
-    print("{:02d}, ".format(i), end='')
+o = 2
+t = 0
+print("01", end='')
+while (t < 9):
+    while (o < 10):
+        print(", {}{}".format(t, o), end='')
+        o = o + 1
+    t = t + 1
+    o = t + 1
+print('\n', end='')
