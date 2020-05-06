f4940fac5ca4caa110552ed05cb33b9adb42c0aa
robertrowe1013 <robertrowe1013@gmail.com>
2020-05-06 04:32:04 +0000

    corrected typo

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
         return None
     for row in matrix:
         for element in row:
-            if elem == row[0]:
+            if element == row[0]:
                 print("{:d}".format(element), end='')
             else:
                 print(" {:d}".format(element), end='')
