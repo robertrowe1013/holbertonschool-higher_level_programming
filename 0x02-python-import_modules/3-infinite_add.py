337f621417a9bd6da1fd18bdc83f29b8b2c1f756
robertrowe1013 <robertrowe1013@gmail.com>
2020-05-06 18:29:39 +0000

    pep format

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
     sum = 0
     i = 1
-    for i in range (i, len(sys.argv)):
+    for i in range(i, len(sys.argv)):
         sum += int(sys.argv[i])
     print("{}".format(sum))
