2cc95e0de0c120816e9362f10754d29e9ea7d36f
robertrowe1013 <robertrowe1013@gmail.com>
2020-05-06 02:50:51 +0000

    task 3

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/pythin3
+def print_reversed_list_integer(my_list=[]):
+    rev_list = my_list[::-1]
+    i = 0
+    while i < len(rev_list):
+        print("{:d}".format(rev_list[i]))
+        i += 1
