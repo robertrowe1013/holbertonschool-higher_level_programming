f07dd80eb7a6a49e660f9f76cd300ca13f530ed0
robertrowe1013 <robertrowe1013@gmail.com>
2020-05-06 04:27:45 +0000

    added null check, added spaces between elements

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
     for row in matrix:
         for element in row:
-            print("{:d}".format(element), end='')
+            if elem == row[0]
+                print("{:d}".format(element), end='')
+            else
+                print("{:d}".format(element), end=' ')
         print()
