f6565bddfddaa19db8c9bf7336cc322a708e144e
robertrowe1013 <robertrowe1013@gmail.com>
2020-05-08 19:07:37 +0000

    pep8 foratting

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_num = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    rom_n = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     prev = 0
     roman_sum = 0
     if type(roman_string) is str:
-        for n in roman_string:        
-            if roman_num[n] > prev:
+        for n in roman_string:
+            if rom_n[n] > prev:
                 roman_sum -= prev
             else:
                 roman_sum += prev
-            prev = roman_num[n]
+            prev = rom_n[n]
         roman_sum += prev
     return roman_sum
