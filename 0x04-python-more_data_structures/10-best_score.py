2cb50b65cf9a21222d22bc4ce05c885f17559fbc
robertrowe1013 <robertrowe1013@gmail.com>
2020-05-08 03:43:18 +0000

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary:
+        highscore = max(a_dictionary, key=lambda x: a_dictionary[x])
+        return highscore
+    else:
+        return None
