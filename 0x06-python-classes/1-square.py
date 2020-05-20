301653bea45e18abf109f077d07dbb5b3fccedca
robertrowe1013 <robertrowe1013@gmail.com>
2020-05-20 02:50:12 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a square.
+
+"""
+
+
+class Square:
+    """A plane figure with four equal length sides and four right angles.
+
+    """
+    def __init__(self, size):
+        """length of size.
+
+        """
+        self.__size = size
