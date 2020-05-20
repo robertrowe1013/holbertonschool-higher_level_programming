e1110837de328eb804c74f4cca61e44a7b883292
robertrowe1013 <robertrowe1013@gmail.com>
2020-05-20 03:46:34 +0000

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a square.
+
+"""
+
+
+class Square:
+    """A plane figure with four equal length sides and four right angles.
+
+    """
+    def __init__(self, size=0):
+        """length of size.
+
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        return (self.__size ** 2)
