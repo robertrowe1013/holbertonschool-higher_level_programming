acd9bde25fa04fba159e271a5e26c43351111df9
robertrowe1013 <robertrowe1013@gmail.com>
2020-05-22 07:13:52 +0000

    task 4, no tests

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_square(size):
+    try:
+        size += 0
+    except TypeError:
+        raise TypeError("size must be an integer")
+    if (type(size) == float) & (size < 0):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for i2 in range(size):
+            print("#", end='')
+        print("")
