209dae01d7a08e7275480b16d8fd5296c60a2068
robertrowe1013 <robertrowe1013@gmail.com>
2020-05-22 06:10:21 +0000

    task two, overwriting original matrix still

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    new_matrix = matrix.copy()
+    for row in range(len(new_matrix) - 1):
+        if len(new_matrix[row]) != len(new_matrix[row + 1]):
+                raise TypeError("Each row of the matrix must have the same size")
+    for i in range(len(new_matrix)):
+        for i2 in range(len(new_matrix[i])):
+            try:
+                new_matrix[i][i2] = round((new_matrix[i][i2] / div), 2)
+            except ZeroDivisionError:
+                raise ZeroDivisionError("division by zero")
+            except TypeError:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    return new_matrix
