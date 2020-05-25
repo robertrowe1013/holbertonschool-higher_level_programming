1fbc5c4d974ccb3ec08c195fffe26dce3e217008
robertrowe1013 <robertrowe1013@gmail.com>
2020-05-25 01:28:14 +0000

    add doc and split long error message

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+"""divide a matrix"""
+
+
 def matrix_divided(matrix, div):
+    """divide a matrix"""
     new_matrix = matrix.copy()
     for row in range(len(new_matrix) - 1):
         if len(new_matrix[row]) != len(new_matrix[row + 1]):
-                raise TypeError("Each row of the matrix must have the same size")
+                raise TypeError("Each row of the matrix "
+                                "must have the same size")
     for i in range(len(new_matrix)):
         for i2 in range(len(new_matrix[i])):
             try:
@@ -11,5 +16,6 @@ def matrix_divided(matrix, div):
             except ZeroDivisionError:
                 raise ZeroDivisionError("division by zero")
             except TypeError:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) "
+                                "of integers/floats")
     return new_matrix
