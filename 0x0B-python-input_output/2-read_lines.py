20b7d4e50cc0119d0d1b1aca23869a3a3421de8d
robertrowe1013 <robertrowe1013@gmail.com>
2020-06-03 20:12:58 +0000

    task 2 incomplete

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""read n lines"""
+
+
+def read_lines(filename="", nb_lines=0):
+    with open(filename, encoding="UTF8") as my_file:
+        if nb_lines < 1:
+            print(my_file.read())
+        elif nb_lines > len(my_file.readlines()):
+            print(my_file.read())
+        else:
+            lines = my_file.readlines()
+            print(lines[:nb_lines])
