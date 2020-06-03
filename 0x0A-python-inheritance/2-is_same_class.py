2303841f17e4066c812c8022a2e784ee00bf021b
robertrowe1013 <robertrowe1013@gmail.com>
2020-06-03 03:13:06 +0000

    task 2 incomplete

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""exact same object"""
+
+
+def is_same_class(obj, a_class):
+    """exact same object"""
+    if isinstance(obj, a_class):
+        return True
+    return False
