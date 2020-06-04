c0e38e6ac007359936245060f37a1e18f17aa687
robertrowe1013 <robertrowe1013@gmail.com>
2020-06-04 09:02:50 +0000

    splits that don't end in spaces

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,7 +6,10 @@ def text_indentation(text):
     """text conversion"""
     if type(text) != str:
         raise TypeError("text must be a string")
-    new_text = text.replace(". ", ".\n\n")
-    new_text = new_text.replace("? ", "?\n\n")
-    new_text = new_text.replace(": ", ":\n\n")
+    new_text = text.replace(". ", ".")
+    new_text = new_text.replace("? ", "?")
+    new_text = new_text.replace(": ", ":")
+    new_text = new_text.replace(".", ".\n\n")
+    new_text = new_text.replace("?", "?\n\n")
+    new_text = new_text.replace(":", ":\n\n")
     print(new_text, end='')
