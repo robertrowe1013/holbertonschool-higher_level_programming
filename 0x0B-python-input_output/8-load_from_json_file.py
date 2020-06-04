bc62774562778f7c8d782d96fba9917357245900
robertrowe1013 <robertrowe1013@gmail.com>
2020-06-04 18:17:45 +0000

    task 8

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""create object from JSON"""
+
+
+def load_from_json_file(filename):
+    """create object from JSON"""
+    import json
+    with open(filename) as my_file:
+        return json.load(my_file)
+        
