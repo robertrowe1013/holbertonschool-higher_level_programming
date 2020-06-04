cdda87e585c897ae7c190510353e266278b903a6
robertrowe1013 <robertrowe1013@gmail.com>
2020-06-04 11:28:33 +0000

    comment code to most along in checker

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -3,11 +3,11 @@
 
 
 def read_lines(filename="", nb_lines=0):
+    "read n lines"
     with open(filename, encoding="UTF8") as my_file:
         if nb_lines < 1:
             print(my_file.read())
         elif nb_lines > len(my_file.readlines()):
             print(my_file.read())
         else:
-            lines = my_file.readlines()
-            print(lines[:nb_lines])
+            
