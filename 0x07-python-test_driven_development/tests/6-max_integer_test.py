9f0b2fa6dfdcc016244fd5b0d1fa0cc4bf279e46
robertrowe1013 <robertrowe1013@gmail.com>
2020-06-04 09:47:41 +0000

    successful tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -9,4 +9,9 @@ class Test6(unittest.TestCase):
     """test task 6"""
 
     def test_max(self):
-        self.assertEqual(max_int([1, 2]), 2)    
+        self.assertEqual(max_int([1, 2]), 2)
+        self.assertEqual(max_int([2, 1]), 2)
+        self.assertEqual(max_int([1, 2, 0]), 2)
+        self.assertEqual(max_int([-1, 2]), 2)
+        self.assertEqual(max_int([-1, -2]), -1)
+        self.assertEqual(max_int([2]), 2)
