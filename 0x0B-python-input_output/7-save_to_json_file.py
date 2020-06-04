bc9bd56100d54f2838dd95d56855ab274a15189a
robertrowe1013 <robertrowe1013@gmail.com>
2020-06-04 15:55:49 +0000

    missing last para

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -6,4 +6,4 @@ def save_to_json_file(my_obj, filename):
     """write object with json"""
     import json
     with open(filename, "w") as my_file:
-        my_file.write(json.dumps(my_obj)
+        my_file.write(json.dumps(my_obj))
