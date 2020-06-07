0ebba81373f4c14672f8de119d1b22664e1d7a9d
robertrowe1013 <robertrowe1013@gmail.com>
2020-06-07 15:07:07 +0000

    added validators

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -12,3 +12,18 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def int_validator(self, name, value):
+        """int validator"""
+        if type(value) != int:
+            raise TypeError("{:s} must be an integer".format(name))
+
+    def gtz_validator(self, name, value):
+        """greater than zero error"""
+        if value < 1:
+            raise ValueError("{:s} must be > 0".format(name))
+
+    def goet_validator(self, name, value):
+        """greater than or equal to error"""
+        if value < 0:
+            raise ValueError("{:s} must be >= 0".format(name))
