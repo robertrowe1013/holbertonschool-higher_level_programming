5a9e4b35974b96ccbbba1f05eb5b772be0ed1b4c
robertrowe1013 <robertrowe1013@gmail.com>
2020-06-09 14:30:31 +0000

    task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,3 +8,18 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """inherit from rectangle, height and width are size"""
         super().__init__(height=size, width=size, x=x, y=y, id=id)
+        self.size = size
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size setter"""   
+        self.int_validator("width", value)
+        self.gtz_validator("width", value)
+        self.width = value
+        self.height = value
+        self.__size = value
