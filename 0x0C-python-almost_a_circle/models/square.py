07ca4e0bef5675b38e3d0a3f0bc1203a775db9db
robertrowe1013 <robertrowe1013@gmail.com>
2020-06-09 19:15:06 +0000

    pep8

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,6 +5,7 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """squares are rectangles"""
+
     def __init__(self, size, x=0, y=0, id=None):
         """inherit from rectangle, height and width are size"""
         super().__init__(height=size, width=size, x=x, y=y, id=id)
@@ -17,9 +18,31 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-        """size setter"""   
+        """size setter"""
         self.int_validator("width", value)
         self.gtz_validator("width", value)
-        self.width = value
-        self.height = value
+        self.__width = value
+        self.__height = value
         self.__size = value
+
+    def update(self, *args, **kwargs):
+        """updater"""
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "id":
+                    super().__init__(height=self.size,
+                                     width=self.size, id=value)
+                if key == "size":
+                    self.__size = value
+                if key == "x":
+                    self.__x = value
+                if key == "y":
+                    self.__y = value
+        if len(args) >= 1:
+            super().__init__(width=self.size, height=self.size, id=args[0])
+        if len(args) >= 2:
+            self.__size = args[1]
+        if len(args) >= 3:
+            self.__x = args[2]
+        if len(args) == 4:
+            self.__y = args[3]
