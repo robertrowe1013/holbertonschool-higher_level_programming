d157b33305bb657bd94092afb21e9003c3f29044
robertrowe1013 <robertrowe1013@gmail.com>
2020-06-09 03:30:25 +0000

    up to task 10

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,11 +7,11 @@ class Rectangle(Base):
     """retangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """init rectangle"""
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
         super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -21,6 +21,8 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """width setter"""
+        self.int_validator("width", value)
+        self.gtz_validator("width", value)
         self.__width = value
 
     @property
@@ -31,6 +33,8 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """height setter"""
+        self.int_validator("height", value)
+        self.gtz_validator("height", value)
         self.__height = value
 
     @property
@@ -41,6 +45,8 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """x setter"""
+        self.int_validator("x", value)
+        self.goet_validator("x", value)
         self.__x = value
 
     @property
@@ -51,4 +57,54 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """y setter"""
+        self.int_validator("y", value)
+        self.goet_validator("y", value)
         self.__y = value
+
+    def area(self):
+        """area func"""
+        return self.__width * self.__height
+
+    def display(self):
+        """print rectangle"""
+        print_string = ""
+        for i in range(self.y):
+            print_string += "\n"
+        for i2 in range(self.height):
+            for i3 in range(self.x):
+                print_string += " "
+            for i4 in range(self.width):
+                print_string += "#"
+            print_string += "\n"
+        print(print_string, end='')
+
+    def __str__(self):
+        """str override"""
+        return ("[{:s}] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
+            self.__class__.__name__, self.id, self.__x,
+            self.__y, self.__width, self.__height))
+
+    def update(self, *args, **kwargs):
+        """updater"""
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "id":
+                    super().__init__(value)
+                if key == "width":
+                    self.__width = value
+                if key == "height":
+                    self.__height = value
+                if key == "x":
+                    self.__x = value
+                if key == "y":
+                    self.__y = value
+        if len(args) >= 1:
+            super().__init__(args[0])
+        if len(args) >= 2:
+            self.__width = args[1]
+        if len(args) >= 3:
+            self.__height = args[2]
+        if len(args) >= 4:
+            self.__x = args[3]
+        if len(args) == 5:
+            self.__y = args[4]
