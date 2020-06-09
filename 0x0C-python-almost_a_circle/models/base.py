53cc514530546a21f6caea493dee2965a1341b2a
robertrowe1013 <robertrowe1013@gmail.com>
2020-06-09 19:08:47 +0000

    pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """base class"""
 
+
 class Base:
     """base class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
         """constructor"""
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
