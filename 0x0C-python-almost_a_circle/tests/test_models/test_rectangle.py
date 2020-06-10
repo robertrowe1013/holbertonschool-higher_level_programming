eaa3f6b53d1537437deb1ec836068de39b3f0d8f
robertrowe1013 <robertrowe1013@gmail.com>
2020-06-10 00:26:43 +0000

    rectangle tests partial

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python
+"""rectangel tests"""
+import unittest
+from models.rectangle import Rectangle
+
+class TestRectangleMethods(unittest.TestCase):
+    """rectangle tests"""
+    def test_rectangle(self):
+        """retangle tests"""
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 1)
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.id, 2)
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r3.id, 12)
+        with self.assertRaises(TypeError):
+            Rectangle(10, "2")
