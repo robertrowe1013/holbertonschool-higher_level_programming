749c1f6a1ddffb825cfc5d4df8153c47e24a8b49
robertrowe1013 <robertrowe1013@gmail.com>
2020-06-10 01:49:47 +0000

    cleaned up layout

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -8,30 +8,36 @@ class TestRectangleMethods(unittest.TestCase):
     """rectangle tests"""
 
     def test_rectangle(self):
-        """retangle tests"""
+        """retangle test sucesses"""
         r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r3.id, 12)
         r4 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r4.id, 12)
+
+    def test_rectangle_te(self):
+        """rectangle type errors"""
         with self.assertRaises(TypeError):
             Rectangle(10, "2")
         with self.assertRaises(TypeError):
             Rectangle("10", 2)
         with self.assertRaises(TypeError):
             Rectangle(10, None)
-        with self.assertRaises(ValueError):
-            Rectangle(10, -2)
         with self.assertRaises(TypeError):
             Rectangle(1.4, 2)
         with self.assertRaises(TypeError):
             Rectangle(1, 1, "1", 1, 1)
         with self.assertRaises(TypeError):
             Rectangle(1, 1, 1, "1", 1)
-        with self.assertRaises(ValueError):
-            Rectangle(1, 1, -1, 1, 1)
-        with self.assertRaises(ValueError):
-            Rectangle(1, 1, 1, -1, 1)
         with self.assertRaises(TypeError):
             Rectangle(1, 1, 1, 1, 1, 1)
         with self.assertRaises(TypeError):
             Rectangle(depth=1)
+
+    def test_rectangle_ve(self):
+        """rectangle value errors"""
+        with self.assertRaises(ValueError):
+            Rectangle(10, -2)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 1, -1, 1, 1)
+        with self.assertRaises(ValueError):
+            Rectangle(1, 1, 1, -1, 1)
