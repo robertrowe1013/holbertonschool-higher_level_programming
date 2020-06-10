745de5a95646cc02b6f7c76b96ffc7c6571224b7
robertrowe1013 <robertrowe1013@gmail.com>
2020-06-10 02:53:04 +0000

    pep8 tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """base tests"""
 import unittest
+import pep8
 from models.base import Base
 
 
@@ -17,3 +18,14 @@ class TestBaseMethods(unittest.TestCase):
         self.assertEqual(b2.id, 2)
         self.assertEqual(b3.id, 12)
         self.assertEqual(b4.id, 3)
+
+
+class TestCodeFormat(unittest.TestCase):
+
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide()
+        result = pep8style.check_files(['models/base.py', 'models/rectangle.py',
+                                        'models/square.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
