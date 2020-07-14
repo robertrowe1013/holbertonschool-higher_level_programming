94c6d544fabb1e9de566a76a90a7bdc31a5610bb
robertrowe1013 <robertrowe1013@gmail.com>
2020-07-14 09:25:03 +0000

    task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- create database and table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    UNIQUE(id)
+);
