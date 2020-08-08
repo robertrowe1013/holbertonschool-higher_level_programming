33a6b6991be2648ba9bc81319ac507e60c1cf5f5
robertrowe1013 <robertrowe1013@gmail.com>
2020-08-08 17:44:34 +0000

    add port

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,6 +8,7 @@ def select_states():
     """ access database print states """
     db = MySQLdb.connect(
         host='localhost',
+        port=3306,
         user=argv[1],
         passwd=argv[2],
         db=argv[3]
