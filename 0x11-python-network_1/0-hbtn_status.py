5601f50fb6a0ba34c9c8b3abe58accb34dd559d5
robertrowe1013 <robertrowe1013@gmail.com>
2020-08-24 08:17:08 +0000

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,9 +7,8 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         bc = response.read()
         bt = type(response.read())
-        test = response.read()
+        bu = bc.decode(encoding='UTF-8')
         print('Body response:')
         print('\t- type {}'.format(bt))
         print('\t- content: {}'.format(bc))
-        print('\t- utf8 content: {}')
-        print(test)
+        print('\t- utf8 content: {}'.format(bu))
