6274651d3dfa04e8fb6ba7d2c1ba4bc8fd253ad9
robertrowe1013 <robertrowe1013@gmail.com>
2020-09-22 20:03:19 +0000

    semi

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
-  console.log(data.toString());
+    console.log(data.toString());
   }
 });
