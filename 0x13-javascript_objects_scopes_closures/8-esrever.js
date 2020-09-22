58488ac104dd5b3fa7bef268e9bae1065b36c728
robertrowe1013 <robertrowe1013@gmail.com>
2020-09-22 04:06:01 +0000

    task 8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+    let temp;
+    let i;
+    let j = list.length - 1;
+
+    for (i = 0; i < (list.length / 2); i++, j--) {
+        temp = list[i];
+        list[i] = list[j];
+        list[j] = temp;
+    }
+    return list;
+}
