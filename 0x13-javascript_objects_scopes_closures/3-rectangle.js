5686a2fe054cc593e4781280081fd1570d87f978
robertrowe1013 <robertrowe1013@gmail.com>
2020-09-22 01:16:48 +0000

    3-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+let column;
+let row;
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    for (column = 0; column < this.height; column++) {
+      for (row = 0; row < this.width; row++) {
+        process.stdout.write("X")
+      }
+      process.stdout.write("\n")
+    }
+  }
+}
+module.exports = Rectangle;
