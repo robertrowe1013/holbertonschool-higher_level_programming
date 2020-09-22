f719ac2ebf0399d491bfaad8a1f487411a50edc9
robertrowe1013 <robertrowe1013@gmail.com>
2020-09-22 04:07:10 +0000

    semi format

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    let temp;
-    let i;
-    let j = list.length - 1;
+  let temp;
+  let i;
+  let j = list.length - 1;
 
-    for (i = 0; i < (list.length / 2); i++, j--) {
-        temp = list[i];
-        list[i] = list[j];
-        list[j] = temp;
-    }
-    return list;
-}
+  for (i = 0; i < (list.length / 2); i++, j--) {
+    temp = list[i];
+    list[i] = list[j];
+    list[j] = temp;
+  }
+  return list;
+};
