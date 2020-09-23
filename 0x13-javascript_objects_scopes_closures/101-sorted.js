cbb2dc3c6e4946c4e77a1d40a93b56c9704ac36a
robertrowe1013 <robertrowe1013@gmail.com>
2020-09-23 15:14:10 +0000

    task 101

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const newDict = {};
+let key;
+let newKey;
+for (key in dict) {
+  if (dict[key] in dict) continue;
+  else newDict[dict[key]] = [];
+}
+for (key in dict) {
+  newKey = dict[key];
+  newDict[newKey].push(key);
+}
+console.log(newDict);
