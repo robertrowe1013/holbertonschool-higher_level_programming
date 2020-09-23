cc8ac381b0cffb507bd55e53a85f18cb093f6324
robertrowe1013 <robertrowe1013@gmail.com>
2020-09-23 04:49:38 +0000

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -10,6 +10,18 @@ request(options, (err, res, body) => {
   if (err) {
     console.log(err);
   } else {
-
+    let taskId;
+    const json = JSON.parse(body);
+    const newDict = {};
+    for (taskId = 1; taskId < json.length; taskId++) {
+      if (json[taskId].completed === true) {
+        if (newDict[json[taskId].userId]) {
+          newDict[json[taskId].userId] += 1;
+        } else {
+          newDict[json[taskId].userId] = 1;
+        }
+      }
+    }
+    console.log(newDict);
   }
 });
