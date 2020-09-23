5b942b49fca6ffedffde71d74b8545600737e3a2
robertrowe1013 <robertrowe1013@gmail.com>
2020-09-23 00:54:51 +0000

    tsk 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+
+const options = {
+  url: 'https://swapi-api.hbtn.io/api/films/' + process.argv[2],
+  method: 'GET'
+};
+request(options, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
