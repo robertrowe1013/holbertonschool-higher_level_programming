b70f1c29bc5bce34d0e7c8a9b2212c8079984ec9
robertrowe1013 <robertrowe1013@gmail.com>
2020-09-23 07:05:12 +0000

    task 6 retry

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -13,7 +13,7 @@ request(options, (err, res, body) => {
     let taskId;
     const json = JSON.parse(body);
     const newDict = {};
-    for (taskId = 1; taskId < json.length; taskId++) {
+    for (taskId = 0; taskId < json.length; taskId++) {
       if (json[taskId].completed === true) {
         if (newDict[json[taskId].userId]) {
           newDict[json[taskId].userId] += 1;
